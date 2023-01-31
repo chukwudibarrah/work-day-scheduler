ad6e501f37fd2b10d8f710b99af7ee3fefd470a1
chukwudibarrah <ceebrrh@gmail.com>
2023-01-31 09:17:40 +0000

    Updated save button sizes to show on schedule

--- assets/js/style.js
+++ assets/js/style.js
@@ -33,15 +33,18 @@ $.each(hoursArray, function (index, value) {
   var eachHour = parseFloat(index);
 
   var saveBtn = $("<button>");
-  $("button").addClass("saveBtn");
+  $("button").addClass("saveBtn submit");
   timeRow.append(saveBtn);
+
+  buttonIcon = ('<i class="fa-solid fa-floppy-disk fa-lg"></i>')
+  saveBtn.append(buttonIcon);
 
   if (eachHour === currentHour) {
     timeRow.addClass("present");
   } else if (eachHour < currentHour) {
-    timeRow.addClass("future");
-  } else {
     timeRow.addClass("past");
+  } else {
+    timeRow.addClass("future");
   }
 
   var textArea = $('<textarea>');
@@ -50,6 +53,64 @@ $.each(hoursArray, function (index, value) {
 });
 
 
+// $('saveBtn').on('click', function() {
+//   saveSettings();
+// loadSettings();
+// });
+
+// function loadSettings() {
+//   $('textarea').val()(localStorage.textarea);
+// }
+
+// function saveSettings() {
+  
+
+
+//   localStorage.textarea = $('input[name="textarea"]').val();
+// }
+
+
+
+// function handleEntrySubmit() {
+//   var userEntry = $('input[name="textarea"]').val();
+//   if (!userEntry) {
+//     alert("You cannot save a blank entry");
+//   } else {
+//     alert("Success", "Your entry was saved.");
+//     return;
+//   }
+// }
+// dayBlock.on('submit', handleEntrySubmit);
+
+
+renderLastRegistered();
+
+function renderLastRegistered() {
+  var userEntry = localStorage.getItem("userEntry");
+
+  if (!userEntry) {
+    return;
+  }
+
+  textArea.text = userEntry;
+}
+
+$('saveBtn').on("click", function(event) {
+  event.preventDefault();
+
+  var userEntry = localStorage.theEntry;
+
+  var theText = $('input[name="textarea"]').val();
+
+  if (userEntry === "") {
+    displayMessage("error", "Entry cannot be blank");
+  } else {
+    alert("Success", "Saved successfully");
+
+    localStorage.setItem("userEntry", theText);
+    renderLastRegistered();
+  }
+});
 
 
 });
